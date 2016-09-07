634bd8502cc470e35b7c25adb6a274358022c15f
charlines <charline@saagie.com>
2016-09-07 11:25:49 +0200

    Correction fonctions

--- impala_querying.R
+++ impala_querying.R
@@ -10,18 +10,20 @@ drv <- JDBC(driverClass = "org.apache.hive.jdbc.HiveDriver",
             identifier.quote="`")
 
 # Connection to impala on the default database
-impalaConnectionUrl <- "jdbc:hive2://datanode1dns:port/;auth=noSasl"
-conn <- dbConnect(drv, dbname = "default", impalaConnectionUrl)
+impalaConnectionUrl <- "jdbc:hive2://IP:Port/;auth=noSasl"
+conn <- dbConnect(drv, impalaConnectionUrl)
 
-# Show tables
+# Show tables (all databases)
 dbListTables(conn) 
-#or 
+# Show tables (the database "default")
 dbGetQuery(conn, "show tables")
 
-# Get all elements of a table
-d <- dbReadTable(conn, "table_name")
-#or
-d <- dbGetQuery(conn, "select * from table_name")
+# Get all elements of a table (in database "default")
+dbReadTable(conn,"table_name")
+# OR
+dbGetQuery(conn, "SELECT * FROM table_name")
+# Get all elements of a table (other that the database "default")
+dbReadTable(conn,"nameBDD.table_name")
 
 # Create a table in parquet format
 dbSendUpdate(conn, "CREATE TABLE table_name (attribute1 string, attribute2 int) STORED AS PARQUET")
@@ -29,3 +31,5 @@ dbSendUpdate(conn, "CREATE TABLE table_name (attribute1 string, attribute2 int) 
 # Insert data in a table
 dbGetQuery(conn, "INSERT INTO table_name VALUES ('test', 1)")
 
+# Refresh Tables
+dbGetQuery(conn, "INVALIDATE METADATA")
